808c5e8b56d9e3c11a390cc3d4a218986160277a
stevebstorm <steve.lorent@gmail.Com>
2022-10-18 10:39:41 +0200

    push

---
+++ BlazorWASMDemo/Pages/Quizz.cs
@@ -0,0 +1,46 @@
+﻿namespace BlazorWASMDemo.Pages
+{
+    public partial class Quizz
+    {
+        public string Username { get; set; }
+        public List<string> Questions{ get; set; }
+        public List<string> Answers{ get; set; }
+        public int pos { get; set; }
+
+        public string InfoPos { get; set; }
+        public string CurrentQuestion { get; set; }
+
+        public Quizz()
+        {
+            Answers = new List<string>();
+            Questions = new List<string>
+            {
+                "Aimes tu les pizzas ?",
+                "As tu lu tout les livre LOTR ?",
+                "Ce quizz sert il a quelque chose ?",
+                "Il est chiant lui",
+                "il veut rajouter des questions"
+            };
+            pos = 1;
+            CurrentQuestion = Questions[0];
+            InfoPos = "Voici la question n°" + pos;
+        }
+
+        public void Answer(string rep)
+        {
+            pos++;
+            InfoPos = "Voici la question n°" + pos;
+            Answers.Add(rep);
+            if(pos > Questions.Count())
+            {
+                CurrentQuestion = "";
+                InfoPos = "Vous avez terminé le quizz";
+            }
+            else
+            {
+                CurrentQuestion = Questions[pos-1];
+            }
+        }
+
+    }
+}
---
+++ BlazorWASMDemo/Services/GameService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorWASMDemo.Models;
+
+namespace BlazorWASMDemo.Services
+{
+    public class GameService
+    {
+        private List<Jeu> Games { get; set; }
+
+        public GameService()
+        {
+            Games = new List<Jeu>
+            {
+                new Jeu{ Id = 1, Name = "The Ascent", Genre = "Shooter", Description = "C'est bien"}
+            };
+        }
+
+        public List<Jeu> GetAll()
+        {
+            return Games;
+        }
+
+        public void Add(Jeu jeu)
+        {
+            Games.Add(jeu);
+        }
+
+
+    }
+}
